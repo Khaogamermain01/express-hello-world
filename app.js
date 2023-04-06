423047795e28d5946c56c28753fa5899fec91b01
Khaogamermain01 <khaogamertest01@gmail.com>
2023-04-07 00:01:11 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+app.use(cors());
 
 app.get("/", (req, res) => res.type('html').send(html));
 
